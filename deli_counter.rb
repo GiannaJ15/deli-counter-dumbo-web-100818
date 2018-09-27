839883233910fbeec1e37d19d3325e5a06cd66fa
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-27 18:08:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+   if katz_deli.length==0 
+    puts "The line is currently empty."
+  end 
+  line_description= "The line is currently:"
+  if katz_deli.length > 0 
+  katz_deli.each_with_index do |person, index|
+    line_description+= " #{index+1}. #{person}"
+  end
+  puts line_description
+end
+end 
+
+def take_a_number
